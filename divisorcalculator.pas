3a2930c13328de0ae22b716b5e65503d62423e8f
Musa Toktas <mdeepst@gmail.com>
2016-12-29 01:37:32 +0200

    Create divisorcalculator.pas

---
+++ divisorcalculator.pas
@@ -0,0 +1,11 @@
+program untitled;
+var 
+a,b: integer;
+begin
+writeln('write the number that you would like to find the divisors')
+readln(a);
+for b:=1 to do
+if a mod b=0 then writeln(b);
+
+readln;
+end
