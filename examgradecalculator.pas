bb9c408a8dfb3062732f69df96eed9b22a6244f2
Musa Toktas <mdeepst@gmail.com>
2016-12-29 02:53:41 +0200

    Create examgradecalculator.pas
    It is also very simple logic program

---
+++ examgradecalculator.pas
@@ -0,0 +1,19 @@
+program untitled;
+var
+n:Integer;
+begin
+writeln('Insert your test score');
+readln(n);
+if n>=50 then
+   if n>=75 then
+   writeln('Your Grade is A')
+   else
+   writeln('Your Grade is B')
+else
+    if n>=25 then
+    writeln('Your Grade is C')
+    else
+    writeln('Your Grade is D');
+
+readln();
+end.
