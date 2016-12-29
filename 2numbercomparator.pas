3d4f9bb9cb9996babfb8e61cf196f0ac2e3073d7
Musa Toktas <mdeepst@gmail.com>
2016-12-29 02:46:20 +0200

    Create 2numbercomparator.pas
    Created and tested simple program

---
+++ 2numbercomparator.pas
@@ -0,0 +1,14 @@
+program untitled; //I know that it is very silly program but these type programs are understandable for beginners..
+var
+a,b:Integer;
+begin
+writeln('Insert the numbers that you want to compare');
+readln(a,b);
+if a>b then
+writeln(a,' is greater then ',b)
+else if a<b then
+writeln(a,' is smaller than ',b)
+else
+writeln(a,' is equal to ',b);
+readln();
+end.
