e9166a3a49083eaa9bb66c47ce877d8419312372
Musa Toktas <mdeepst@gmail.com>
2016-12-29 20:48:35 +0200

    Create while-do.pas

---
+++ loopsamples/while-do.pas
@@ -0,0 +1,12 @@
+program Untitled; //this program aimed for displaying number 1 to 100 on the screen
+var
+say:Integer;
+begin
+say:=1;
+while say<=100 do
+begin
+writeln(say);
+say:=say+1;
+end;
+readln();
+end.
