b061306257c654a27e4276e43037ad2537b4fb70
Musa Toktas <mdeepst@gmail.com>
2017-01-02 13:37:27 +0200

    if else sample created

---
+++ Loop Samples/if-else.pas
@@ -0,0 +1,19 @@
+program ifelseshape;
+var
+i,k,l:integer;
+begin
+
+i:=5;
+k:=90;
+l:=8 ;
+
+if i*k>l then
+  
+  writeln('i multiply by k bigger than l')
+  
+  else
+  writeln('i*k is not equal to l');
+  
+  
+readln()
+end.
